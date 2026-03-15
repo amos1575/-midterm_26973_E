0c89bb15f55e17b9af506bd037f2c5523514d241
Nkurunziza Amos <nziza.amos1@gmail.com>
2026-03-15 23:42:53 +0200

    Fix LocationRepository ID type from Long to UUID

--- personal_finance_tracker/src/main/java/personal_finance_tracker/personal_finance_tracker/repository/LocationRepository.java
+++ personal_finance_tracker/src/main/java/personal_finance_tracker/personal_finance_tracker/repository/LocationRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
+public interface LocationRepository extends JpaRepository<Location, UUID> {
     List<Location> findByParentLocation(Location parentLocation);
 
     @Query("SELECT l FROM Location l WHERE l.parentLocation.id = :parentId")
-    List<Location> findByParentId(@Param("parentId") Long parentId);
+    List<Location> findByParentId(@Param("parentId") UUID parentId);
 }
